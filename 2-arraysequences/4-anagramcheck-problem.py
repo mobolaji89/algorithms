2378696c9ff64b3ad3de4754607d6211b5387195
Mobolaji David <davidmo@colorado.edu>
2017-02-20 22:59:27 -0800

    add second recommended solution for anagram problem

--- 2-arraysequences/4-anagramcheck-problem.py
+++ 2-arraysequences/4-anagramcheck-problem.py
@@ -18,7 +18,6 @@ So "d go" is an anagram of "God" and "dog" and "o d g".
 """
 
 # My Inital Solution
-
 def anagram(s1,s2):
 
     s1 = s1.lower().replace(" ","")
@@ -36,10 +35,52 @@ anagram("Do g", "god")
 anagram("public relations", "crap built on lies")
 anagram("clint eastwood", "old west action")
 
-# Recommended Solution
+# First Solution
 def anagram(s1, s2):
 
     s1 = s1.replace(' ','').lower()
     s2 = s2.replace(' ','').lower()
 
     return sorted(s1) == sorted(s2)
+
+# Alternate (more optimal) Solution
+def anagram2(s1,s2):
+
+    # Remove spaces and lowercase letters
+    s1 = s1.replace(' ','').lower()
+    s2 = s2.replace(' ','').lower()
+
+    # Edge Case to check if same number of letters
+    if len(s1) != len(s2):
+        return False
+
+    # Create counting dictionary
+    count = {}
+
+    # Fill dictionary for first string (add counts)
+    for letter in s1:
+        if letter in count:
+            count[letter] += 1
+        else:
+            count[letter] = 1
+
+    # Fill dictionary for second string (subtract counts)
+    for letter in s2:
+        if letter in count:
+            count[letter] -= 1
+        else:
+            count[letter] = 1
+
+    # Check that all counts are 0
+    for k in count:
+        if count[k] != 0:
+            return False
+
+    # Otherwise they're anagrams
+    return True
+
+"""
+Note for the second solution: The use of DefaultDict from the Collections 
+module would clean up this code quite a bit, and the final for loop could be built
+into the second for loop, but in the above implementation every step is very clear.
+"""
